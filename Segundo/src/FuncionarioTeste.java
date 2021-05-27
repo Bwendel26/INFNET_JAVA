9d8c617b42e49f97236add1ee532952b7a5cce4c
bwendel26 <bwendel26999@gmail.com>
2021-05-26 21:14:14 -0300

    Adicionando diagramas das ultimas aulas de OO e adicionando arquivos da aula 8 de Fundamentos de Java, modificando o arquivo para ficar no modelo OO

---
+++ Segundo/src/FuncionarioTeste.java
@@ -0,0 +1,14 @@
+import entities.Funcionario;
+
+public class FuncionarioTeste {
+
+	public FuncionarioTeste() {
+		
+		Funcionario funcionario = new Funcionario();
+		
+		funcionario.setNome("Bruno");
+		
+		
+	}
+
+}
