8206d05e47489dd0131f26a1a2cca99c770cfd89
bwendel26 <bwendel26999@gmail.com>
2021-08-09 23:20:50 -0300

    Adicionando tarefas referentes ao TP1

---
+++ 2_Desenvolvimento_de_servicos_em_Nuvem_com_Java/TP1/JCloudTP1/src/main/java/br/edu/infnet/bwendel/JCloudTP1/JCloudTp1Application.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.bwendel.JCloudTP1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JCloudTp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JCloudTp1Application.class, args);
+	}
+
+}
---
+++ 2_Desenvolvimento_de_servicos_em_Nuvem_com_Java/TP1/JCloudTP1/src/test/java/br/edu/infnet/bwendel/JCloudTP1/JCloudTp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.bwendel.JCloudTP1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JCloudTp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
